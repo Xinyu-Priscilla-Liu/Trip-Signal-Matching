de55c1b76e9f2baa8fe81aad0768e45526ff260f
Xinyu-Priscilla-Liu <liuxinyu199005@gmail.com>
2018-06-11 15:57:14 -0400

    This is the first commit to the trip matching task

---
+++ Xinyu_Liu.R
@@ -0,0 +1,129 @@
+#####################
+## load the data
+#####################
+load("C:/Users/Owner/Desktop/Root/mobileTrips.RData") 
+load("C:/Users/Owner/Desktop/Root/obd2Trips.RData") 
+
+# This function is used to calculate the normalized correlation between two signals/series of similar length
+# It takes in two vectors of the same length and return their normalized correlation value when lag = 0
+norm_corfun <- function (a, b) {
+  a. <- a - mean(a) 
+  b. <- b - mean(b)
+  SSa <- sum(a.^2) 
+  SSb <- sum(b.^2) 
+  SaSb <- sqrt(SSa*SSb) 
+  return(sum(a.*b.)/SaSb)
+}
+
+# This function is used to perform first differencing to trips
+# It takes in the Trip dataset, such as mobileTripsTrain or obd2TripsTrain in this case
+first_differencing <- function (Trips) {
+  for (i in 1:length(Trips)) {
+    Trips[[i]]$diff <- ave(Trips[[i]]$speed, FUN=function(x) c(0, diff(x)))
+  }
+  return(Trips)
+}
+
+# This function is used to return required output
+# It takes mobile trip datasets and obd trip datasets, and an additional threshold value as inputs
+# The threshold value is chosen based on how similar you want the returned signals to be.
+result_function <- function (mobileTrips, obdTrips, threshold) {
+  
+  # The following code applies the first differencing function to update both data sets
+  mobileTrips <- first_differencing(mobileTrips)
+  obdTrips <- first_differencing(obdTrips)
+  
+  # Initialize a 3-dimensional array storing all information
+  similarity_array <- array(0, dim = c(length(mobileTrips), length(obdTrips), 3))
+  
+  # We need to compare m * n pairs of trips
+  for (m in 1:length(mobileTrips)){
+    for (n in 1:length(obdTrips)){
+      
+      # Find the shorter trip between the two trips, and save the information on which trip is shorter as the third layer in the array
+      if (nrow(mobileTrips[[m]]) >= nrow(obdTrips[[n]])) {
+        shorter_signal <- obdTrips[[n]]$diff
+        longer_signal <- mobileTrips[[m]]$diff
+        similarity_array[m, n, 3] <- 1   # Use 1 to present the obd trips
+      } else {
+        shorter_signal <- mobileTrips[[m]]$diff
+        longer_signal <- obdTrips[[n]]$diff
+        similarity_array[m, n, 3] <- 2  # Use 2 to present mobile trips
+      }
+      
+      # Assuming the two trips are matching, we can see what are the highest correlation score that these two trips can obtain 
+      # and use this as the similarity measure. The following code creates a vector, the index indicates the potential matching positions
+      # and the value in the vector stores the similarity score
+      counter <- 1
+      align_vector <- numeric(length(longer_signal) - length(shorter_signal) + 1)
+      while (counter < (length(longer_signal) - length(shorter_signal) + 2)) {
+        cut_signal <- longer_signal[counter: (counter + length(shorter_signal) - 1)]
+        align_vector[counter] <- norm_corfun(shorter_signal, cut_signal)
+        counter = counter + 1
+      }
+      
+      # From the align vector above, we obtain the best align position 
+      best_align_position <- which.max(align_vector)
+      if (length(best_align_position) == 0) {
+        best_align_position <- 0  # indicating no position found
+      }
+      
+      # From the align vector above, we obtain the best align value
+      best_align_value <- max(align_vector)
+      
+      # The best align value is saved in the first layer of the array
+      similarity_array[m, n, 1] <- best_align_value
+      
+      # The best align position is saved in the first layer of the array
+      similarity_array[m, n, 2] <- best_align_position  # Use the second layer of the array to store the matching position
+    }
+  }
+  
+  # If there are NaN values, their values are set to zero
+  similarity_array[is.nan(similarity_array)] <- 0
+  
+  # To compare similarity, a threshold value is needed. 
+  # All the similarity scores that are larger than a specific threshold can be considered containing similar patterns.
+  matching_pairs <- which(similarity_array[ , , 1] > threshold, arr.ind = TRUE)  # Need to choose a threshhold
+  
+  # Use the information stored in the array to extract the data frames and output the results in the required format
+  result <- list()
+  for (p in 1:(length(matching_pairs)/2)){
+    first_ind <- matching_pairs[p, "row"]
+    second_ind <- matching_pairs[p, "col"]
+    matching_position <- similarity_array[first_ind, second_ind, 2]
+    shorter_series <- similarity_array[first_ind, second_ind, 3]
+    if (shorter_series == 1){
+      df.1 <- obdTrips[[second_ind]]
+      df.2 <- mobileTrips[[first_ind]][matching_position : (matching_position + length(df.1$diff) - 1), ]
+      result[[p]] <- list(df.1,df.2)
+    } else {
+      df.2 <- mobileTrips[[first_ind]]
+      df.1 <- obdTrips[[second_ind]][matching_position : (matching_position + length(df.2$diff) - 1), ]
+      result[[p]] <- list(df.1,df.2)
+    }
+  }
+  return(result)
+}
+
+# To obtain the final results, we need to call the following:
+output <- result_function(mobileTripsTrain, obd2TripsTrain, 0.3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
